f6533f80f6565f8cc1f0f2a91067f6f0140cbab8
lokeshkp <kplokesh@gmail.com>
2018-06-23 13:52:53 +0530

    git push test

---
+++ src/test/java/com/qa/tests/GitHubTest.java
@@ -0,0 +1,12 @@
+package com.qa.tests;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This class is for test to push code into git hub ...");
+
+	}
+
+}
